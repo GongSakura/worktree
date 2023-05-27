5667a47762a3cd4d29ad51735914f627ff6ed77e
GongSakura <gongsakura@yahoo.com>
2023-05-28 09:49:02 +1000

    feat: prepare script

--- packages/lib/core/processor/error.ts
+++ packages/lib/core/processor/error.ts
@@ -1,11 +1,12 @@
+import chalk from "chalk";
 function captureError(context: any, next: CallableFunction) {
   try {
     next();
   } catch (error:any) {
     //TODO: To come up with a natty solution to show the error message
     
-    console.info(`error capture:\n`,error?.stderr?.toString());
-    console.info(`error:`,error)
+    console.log(chalk.bgRed.bold('error'))
+    console.log(error.message)
   }
 }
 
---
+++ scripts/prepare.js
@@ -0,0 +1,5 @@
+/**
+ * Prepare: change git hooks path and make it excutable
+ */
+
+console.log('TODO:','change git hooks path and make it excutable')
